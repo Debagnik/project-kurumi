b98156adc97424ceff6c378122237fea423232b3
Debagnik <debagnikkar@outlook.com>
2024-11-09 22:41:35 +0530

    Fix: changed error logs to display IP of the paas environments

--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const mongoose = require('mongoose');
 const MAX_RETRY = 3;
 const RETRY_DELAY = 5000;
@@ -11,6 +12,12 @@ const connectDB = async(retryCount = 0) => {
     } catch(error){
         console.log(`Database connection attempt ${retryCount + 1} failed:`, error.message);
         if (retryCount < MAX_RETRY) {
+            var http = require('http');
+            http.get({'host': 'api.ipify.org', 'port': 80, 'path': '/'}, function(resp) {
+                resp.on('data', function(ip) {
+                  console.log("My public IP address is: " + ip);
+                });
+              });
             console.log(`Retrying in ${RETRY_DELAY/1000} seconds...`);
             await new Promise(resolve => setTimeout(resolve, RETRY_DELAY));
             return connectDB(retryCount + 1);
