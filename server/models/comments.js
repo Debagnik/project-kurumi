994190de686ce339dd5a73c0aa66ff2389ca75f2
Debagnik Kar <39880615+Debagnik@users.noreply.github.com>
2025-04-09 00:13:41 +0530

    Feature | Adding Comments form to posts and create Comments table in DB | Issue #61 (#63)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced a comments section on post pages that allows users to
    easily submit feedback with their name and message. Input validations
    help ensure quality entries.
    - Enhanced security measures now protect the comment submission process,
    providing users with a safer and more robust experience.
    - Comments are enabled based on site configuration, ensuring flexibility
    in post interactions.
    	- CSRF protection has been added to secure comment submissions.
    - New routes for posting and deleting comments have been implemented,
    allowing users to manage their feedback effectively.
    - A new schema for comments has been established to manage user
    feedback.
    - **Style**
    - Added new styles for the comments section to enhance layout and
    presentation in the user interface.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    
    
    * issue #61 | Initial commit for adding comments form
    
    * issue #61 | Fix CSRF protection for adding comments form
    
    * issue #61 | Fix review comment and refactoring
    
    * issue #61 | added route to add comment on DB
    
    * issue #61 | fix review comments
    
    * issue #61 | fix build issue
    
    * issue #61 | minor fix
    
    * issue #61 | minor fix 2
    
    * issue #61 | added a delete route to for comments
    
    * issue #61 | fix minor issues
    
    * Issue #61 | Fix user token issues
    
    * issue #61 | fix review comments
    
    * issue #61 | fixed decorative changes
    
    * issue #61 | reformateed the file
    
    * issue #61 | redundant code removal

---
+++ server/models/comments.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const schema = mongoose.Schema;
+const CommentSchema = new schema({
+    commenterName: {
+        type: String,
+        required: true,
+        minlength: [3, 'Commenter name must be at least 3 characters!'],
+        maxlength: [50, 'Commenter name is too long!'],
+        trim: true
+    },
+    commentBody: {
+        type: String,
+        required: true,
+        minlength: [1, 'Comment must be at least 1 character!'],
+        maxlength: [500, 'Comment is too long!'],
+        trim: true
+    },
+    commentTimestamp: {
+        type: Date,
+        default: Date.now,
+        required: true
+    },
+    postId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post',
+        required: true,
+        index: true
+    }
+});
+CommentSchema.index({
+    commenterName: 'text',
+    commentBody: 'text',
+    commentTimestamp: -1
+});
+
+module.exports = mongoose.model('comment', CommentSchema);
--- server/routes/main.js
+++ server/routes/main.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const post = require('../models/posts');
 const siteConfig = require('../models/config');
 const user = require('../models/user');
+const comment = require('../models/comments');
+const csrf = require('csurf');
 
 const jwtSecretKey = process.env.JWT_SECRET;
 const { PRIVILEGE_LEVELS_ENUM, isWebMaster } = require('../../utils/validations');
@@ -39,6 +41,10 @@ if (!jwtSecretKey) {
     // You might want to exit the process gracefully
     process.exit(1);
 }
+
+// adding admin CSRF protection middleware
+const csrfProtection = csrf({ cookie: true });
+router.use(csrfProtection);
 
 //Routes
 /**
@@ -115,20 +121,7 @@ router.get('/contact', (req, res) => {
  */
 router.get('/post/:id', async (req, res) => {
     try {
-        let currentUser = null;
-        const token = req.cookies.token;
-        let userId = null;
-        if (token) {
-            try {
-                const decoded = jwt.verify(token, jwtSecretKey);
-                userId = decoded.userId;
-            } catch (err) {
-                console.error('Invalid token:', err.message);
-            }
-        }
-        if (userId) {
-            currentUser = await user.findById(userId);
-        }
+        const currentUser = await getUserFromCookieToken(req);
 
         let slug = req.params.id;
         const data = await post.findById({ _id: slug });
@@ -147,11 +140,17 @@ router.get('/post/:id', async (req, res) => {
         } else {
             data.author = postAuthor.name;
         }
-        
-        if(currentUser || data.isApproved) {
+
+        const isCommentsEnabled = res.locals.siteConfig.isCommentsEnabled && !!res.locals.siteConfig.cloudflareSiteKey && !!res.locals.siteConfig.cloudflareServerKey;
+        const isCurrentUserAModOrAdmin = currentUser && (currentUser.privilegeLevel === PRIVILEGE_LEVELS_ENUM.ADMIN || currentUser.privilegeLevel === PRIVILEGE_LEVELS_ENUM.MODERATOR);
+        if (currentUser || data.isApproved) {
             res.render('posts', {
                 locals,
-                data
+                data,
+                csrfToken: req.csrfToken(),
+                isCommentsEnabled,
+                commentsData: await getCommentsFromPostId(data._id),
+                currentUser: isCurrentUserAModOrAdmin
             });
         } else {
             res.redirect('/404');
@@ -168,6 +167,34 @@ router.get('/post/:id', async (req, res) => {
         });
     }
 });
+
+const getUserFromCookieToken = async (req) => {
+    let currentUser = null;
+    const token = req.cookies.token;
+    let userId = null;
+    if (token) {
+        try {
+            const decoded = jwt.verify(token, jwtSecretKey);
+            userId = decoded.userId;
+        } catch (err) {
+            console.error('Invalid token:', err.message);
+        }
+    }
+    if (userId) {
+        currentUser = await user.findById(userId);
+    }
+    return currentUser;
+}
+
+const getCommentsFromPostId = async (postId) => {
+    try {
+        const comments = await comment.find({ postId }).sort({ commentTimestamp: -1 });
+        return comments;
+    } catch (error) {
+        console.error('Comment Fetch error', postId, error.message);
+        return [];
+    }
+}
 
 /**
  * POST /
@@ -220,6 +247,95 @@ router.post('/search', async (req, res) => {
             config: res.locals.siteConfig
         });
     }
+});
+
+/**
+ * POST
+ * /posts/post-comments
+ * Add comments to a post
+ */
+router.post('/post/:id/post-comments', async (req, res) => {
+    const { postId, commenterName, commentBody } = req.body;
+    const siteConfig = res.locals.siteConfig;
+    if (!siteConfig.isCommentsEnabled || !siteConfig.cloudflareSiteKey || !siteConfig.cloudflareServerKey) {
+        console.error(403, 'Comments are disabled or Cloudflare keys are not set');
+        return res.status(403).json({ "status": "403", "message": "Comments are disabled or Cloudflare keys are not set" });
+    }
+
+    if (!commenterName || !commentBody) {
+        console.error(400, 'Invalid comment data');
+        return res.redirect(`/post/${postId}`);
+    }
+    if (commentBody.length > 500 || commenterName.length > 50 || commenterName.length < 3 || commentBody.length < 1) {
+        console.error(400, 'Invalid comment data', 'Size mismatch');
+        return res.redirect(`/post/${postId}`);
+    }
+
+    try {
+
+        //verify if post exists before adding comment. If not, return 404. 404 status code indicates the requested resource was not found on the server. 401 status code
+        const existingPost = await post.findById(postId);
+        if (!existingPost) {
+            console.error(404, 'No post found');
+            return res.status(404).json({ "status": "404", "message": "No post found", "postId": postId });
+        }
+
+        const newComment = new comment({
+            postId: postId,
+            commenterName: commenterName,
+            commentBody: commentBody,
+            commentTimestamp: Date.now()
+        });
+        await newComment.save();
+        if (process.env.NODE_ENV === 'production') {
+            console.log({ "status": "200", "message": "Comment added successfully" });
+        } else {
+            console.log({ "status": "200", "message": "Comment added successfully", "comment": newComment });
+        }
+        res.redirect(`/post/${postId}`);
+    } catch (error) {
+        console.error('Error adding comment:', error);
+        if (process.env.NODE_ENV === 'production') {
+            res.status(500).json({ "status": "500", "message": "Unable to add comment at this time" });
+        } else {
+            res.status(500).json({ "status": "500", "message": "Error adding comment at this time", "error": error.message });
+        }
+    }
+
+});
+
+/**
+ * DELETE
+ * /posts/post-comments/:commentId
+ * Delete a comment from a post if the User is Authorized (Only Admin or Moderator)
+ */
+router.post('/post/delete-comment/:commentId', async (req, res) => {
+    const { commentId } = req.params;
+    try {
+        // verify if the comment exists before deleting. If not, redirect to the post page. 404 status to be logged in console
+        const thisComment = await comment.findById(commentId);
+        if (!thisComment) {
+            console.error(404, 'No comment found');
+            return res.status(404).json({ "status": "404", "message": "No comment found", "commentid": commentId });
+        }
+        // check if user is authorized to delete the comment
+        const currentUser = await getUserFromCookieToken(req);
+        const isCurrentUserAModOrAdmin = currentUser && (currentUser.privilegeLevel === PRIVILEGE_LEVELS_ENUM.ADMIN || currentUser.privilegeLevel === PRIVILEGE_LEVELS_ENUM.MODERATOR);
+        if (!isCurrentUserAModOrAdmin) {
+            console.error(403, 'Unauthorized to delete comment');
+            return res.status(403).json({ "status": "403", "message": "Unauthorized to delete comment" });
+        }
+
+        await thisComment.deleteOne()
+        console.log({ "status": "200", "message": "Comment deleted successfully", user: currentUser.username });
+        res.redirect(`/post/${thisComment.postId}`);
+
+    } catch (err) {
+        console.error(500, 'Error deleting comment:', err);
+        return res.status(500).json({ "status": "500", "message": "Error deleting comment" });
+    }
+
 })
+
 
 module.exports = router;
