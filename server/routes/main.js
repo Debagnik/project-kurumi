ec71d18ad4ca3c04629e6bef80b52dbb013a001a
Debagnik <debagnikkar@outlook.com>
2025-04-08 23:56:58 +0530

    issue #61 | reformateed the file

--- server/routes/main.js
+++ server/routes/main.js
@@ -143,13 +143,13 @@ router.get('/post/:id', async (req, res) => {
 
         const isCommentsEnabled = res.locals.siteConfig.isCommentsEnabled && !!res.locals.siteConfig.cloudflareSiteKey && !!res.locals.siteConfig.cloudflareServerKey;
         const isCurrentUserAModOrAdmin = currentUser && (currentUser.privilegeLevel === PRIVILEGE_LEVELS_ENUM.ADMIN || currentUser.privilegeLevel === PRIVILEGE_LEVELS_ENUM.MODERATOR);
-        if(currentUser || data.isApproved) {
+        if (currentUser || data.isApproved) {
             res.render('posts', {
                 locals,
                 data,
                 csrfToken: req.csrfToken(),
                 isCommentsEnabled,
-                commentsData : await getCommentsFromPostId(data._id),
+                commentsData: await getCommentsFromPostId(data._id),
                 currentUser: isCurrentUserAModOrAdmin
             });
         } else {
@@ -170,32 +170,32 @@ router.get('/post/:id', async (req, res) => {
 
 const getUserFromCookieToken = async (req) => {
     let currentUser = null;
-        const token = req.cookies.token;
-        let userId = null;
-        if (token) {
-            try {
-                const decoded = jwt.verify(token, jwtSecretKey);
-                // Check if token is expired
-                if (decoded.exp && Date.now() >= decoded.exp * 1000) {
-                    console.error('Token expired');
-                    return null;
-                }
-                userId = decoded.userId;
-            } catch (err) {
-                console.error('Invalid token:', err.message);
+    const token = req.cookies.token;
+    let userId = null;
+    if (token) {
+        try {
+            const decoded = jwt.verify(token, jwtSecretKey);
+            // Check if token is expired
+            if (decoded.exp && Date.now() >= decoded.exp * 1000) {
+                console.error('Token expired');
+                return null;
             }
+            userId = decoded.userId;
+        } catch (err) {
+            console.error('Invalid token:', err.message);
         }
-        if (userId) {
-            currentUser = await user.findById(userId);
-        }
-        return currentUser;
+    }
+    if (userId) {
+        currentUser = await user.findById(userId);
+    }
+    return currentUser;
 }
 
 const getCommentsFromPostId = async (postId) => {
-    try{
+    try {
         const comments = await comment.find({ postId }).sort({ commentTimestamp: -1 });
         return comments;
-    } catch(error){
+    } catch (error) {
         console.error('Comment Fetch error', postId, error.message);
         return [];
     }
@@ -260,50 +260,50 @@ router.post('/search', async (req, res) => {
  * Add comments to a post
  */
 router.post('/post/:id/post-comments', async (req, res) => {
-    const { postId, commenterName, commentBody} = req.body;
+    const { postId, commenterName, commentBody } = req.body;
     const siteConfig = res.locals.siteConfig;
-    if(!siteConfig.isCommentsEnabled || !siteConfig.cloudflareSiteKey || !siteConfig.cloudflareServerKey){
+    if (!siteConfig.isCommentsEnabled || !siteConfig.cloudflareSiteKey || !siteConfig.cloudflareServerKey) {
         console.error(403, 'Comments are disabled or Cloudflare keys are not set');
-        return res.status(403).json({"status": "403", "message": "Comments are disabled or Cloudflare keys are not set"});
-        }
+        return res.status(403).json({ "status": "403", "message": "Comments are disabled or Cloudflare keys are not set" });
+    }
 
-    if(!commenterName ||!commentBody) {
+    if (!commenterName || !commentBody) {
         console.error(400, 'Invalid comment data');
         return res.redirect(`/post/${postId}`);
     }
-    if(commentBody.length > 500 || commenterName.length > 50 || commenterName.length < 3 || commentBody.length < 1) {
+    if (commentBody.length > 500 || commenterName.length > 50 || commenterName.length < 3 || commentBody.length < 1) {
         console.error(400, 'Invalid comment data', 'Size mismatch');
         return res.redirect(`/post/${postId}`);
     }
 
-    try{
+    try {
 
         //verify if post exists before adding comment. If not, return 404. 404 status code indicates the requested resource was not found on the server. 401 status code
-        const existingPost  = await post.findById(postId);
-        if(!existingPost) {
+        const existingPost = await post.findById(postId);
+        if (!existingPost) {
             console.error(404, 'No post found');
-            return res.status(404).json({"status": "404", "message": "No post found", "postId": postId});
+            return res.status(404).json({ "status": "404", "message": "No post found", "postId": postId });
         }
-        
+
         const newComment = new comment({
             postId: postId,
             commenterName: commenterName,
             commentBody: commentBody,
             commentTimestamp: Date.now()
-    });
+        });
         await newComment.save();
-        if(process.env.NODE_ENV === 'production'){
-            console.log({"status": "200", "message": "Comment added successfully" });
+        if (process.env.NODE_ENV === 'production') {
+            console.log({ "status": "200", "message": "Comment added successfully" });
         } else {
-            console.log({"status": "200", "message": "Comment added successfully", "comment": newComment });
+            console.log({ "status": "200", "message": "Comment added successfully", "comment": newComment });
         }
         res.redirect(`/post/${postId}`);
-    } catch(error){
+    } catch (error) {
         console.error('Error adding comment:', error);
-        if(process.env.NODE_ENV === 'production'){
-            res.status(500).json({"status": "500", "message": "Unable to add comment at this time" });
+        if (process.env.NODE_ENV === 'production') {
+            res.status(500).json({ "status": "500", "message": "Unable to add comment at this time" });
         } else {
-            res.status(500).json({"status": "500", "message": "Error adding comment at this time", "error": error.message });
+            res.status(500).json({ "status": "500", "message": "Error adding comment at this time", "error": error.message });
         }
     }
 
@@ -316,30 +316,30 @@ router.post('/post/:id/post-comments', async (req, res) => {
  */
 router.post('/post/delete-comment/:commentId', async (req, res) => {
     const { commentId } = req.params;
-    try{
+    try {
         // verify if the comment exists before deleting. If not, redirect to the post page. 404 status to be logged in console
         const thisComment = await comment.findById(commentId);
-        if(!thisComment) {
+        if (!thisComment) {
             console.error(404, 'No comment found');
-            return res.status(404).json({"status": "404", "message": "No comment found", "commentid": commentId});
+            return res.status(404).json({ "status": "404", "message": "No comment found", "commentid": commentId });
         }
         // check if user is authorized to delete the comment
         const currentUser = await getUserFromCookieToken(req);
         const isCurrentUserAModOrAdmin = currentUser && (currentUser.privilegeLevel === PRIVILEGE_LEVELS_ENUM.ADMIN || currentUser.privilegeLevel === PRIVILEGE_LEVELS_ENUM.MODERATOR);
-        if(!isCurrentUserAModOrAdmin){
+        if (!isCurrentUserAModOrAdmin) {
             console.error(403, 'Unauthorized to delete comment');
-            return res.status(403).json({"status": "403", "message": "Unauthorized to delete comment" });
+            return res.status(403).json({ "status": "403", "message": "Unauthorized to delete comment" });
         }
 
         await thisComment.deleteOne()
-        console.log({"status": "200", "message": "Comment deleted successfully", user: currentUser.username });
+        console.log({ "status": "200", "message": "Comment deleted successfully", user: currentUser.username });
         res.redirect(`/post/${thisComment.postId}`);
-        
-    }catch(err){
+
+    } catch (err) {
         console.error(500, 'Error deleting comment:', err);
-        return res.status(500).json({"status": "500", "message": "Error deleting comment" });
+        return res.status(500).json({ "status": "500", "message": "Error deleting comment" });
     }
-    
+
 })
 
 
